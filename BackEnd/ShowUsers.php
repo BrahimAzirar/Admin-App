2ab78282347f6c20bbcaa83d58e0a52ef0d98910
Brahim_Azirar <brahimazirar59@gmail.com>
2023-01-05 17:09:42 +0100

    get data from database 30%

--- BackEnd/ShowUsers.php
+++ BackEnd/ShowUsers.php
@@ -1,3 +1,29 @@
 <?php
 
-    
+    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header("Content-Type: JSON");
+
+    $connect = new PDO("mysql:host=localhost:3308; dbname=exo; ", "root", "");
+    $resp = [];
+
+    $name = $_POST['name'];
+    $val = $_POST['val'];
+
+    if ($val) {
+        $GetUsers = $connect -> prepare ("SELECT * FROM users WHERE $name LIKE '%$val%'");
+        $GetUsers -> execute();
+
+        foreach ($GetUsers as $value) {
+            array_push($resp, [
+                "id" => $value["id"],
+                "FirstName" => $value["FirstName"],
+                "LastName" => $value["LastName"],
+                "Email" => $value["Email"],
+                "_Password" => $value["_Password"]
+            ]);
+        };
+
+        echo json_encode($resp, JSON_PRETTY_PRINT);
+    } else {
+        echo json_encode([], JSON_PRETTY_PRINT);
+    }
--- Show_Info/UsersInfo.js
+++ Show_Info/UsersInfo.js
@@ -1,25 +1,68 @@
-import React, { useState } from 'react';
-
-import Menu from '../Menu';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function UsersInfo() {
 
   const [InputName, setInputName] = useState('FirstName');
+  const [tbodyContent, settbodyContent] = useState([]);
+
+  const mySearchForm = useRef();
+  const Target = useRef();
+
+
+  useEffect(() => {
+    
+  }, [InputName]);
+
+  function GetData(e) {
+    const Mydata = new URLSearchParams();
+
+    Mydata.append('name', InputName);
+    Mydata.append('val', e.target.value);
+
+    console.log(Array.from(Mydata));
+
+    fetch('http://localhost/appadmin/src/BackEnd/ShowUsers.php', {
+      method: "POST",
+      body: Mydata
+    })  .then(resp => resp.json())
+        .then(data => {
+          settbodyContent(data.map(ele => {
+            return (
+              <tr>
+                <td>{ele.id}</td>
+                <td>{ele.FirstName}</td>
+                <td>{ele.LastName}</td>
+                <td>{ele.Email}</td>
+                <td>{ele._Password}</td>
+                <td>
+                  <a href="#">Edite</a>
+                  <a href="#">Delete</a>
+                </td>
+              </tr>
+            );
+          }));
+        })
+        .catch(err => console.log(err));
+  }
+
+  function ChangeInputName(e) {
+    setInputName(e.target.value);
+  }
 
   return (
     <div className='col-10 h-75 align-self-center p-3 d-flex flex-column justify-content-between'>
       <h2 className='text-center'>Admin Page</h2>
-      <form className='h-25 row align-content-center justify-content-center'>
+      <form ref={mySearchForm} className='h-25 row align-content-center justify-content-center'>
         <div className='col-6 mx-2'>
-          <input type="text" name={InputName} className='form-control p-1' />
+          <input type="text" ref={Target} name={InputName} className='form-control p-1' onChange={GetData} />
         </div>
         <div className='col-2 mx-2'>
-          <select className='form-select p-1'>
-            <option value="UserId">id</option>
+          <select className='form-select p-1' onClick={ChangeInputName}>
+            <option value="id">id</option>
             <option value="FirstName">First Name</option>
             <option value="LastName">Last Name</option>
             <option value="Email">Email</option>
-            <option value="Password">Password</option>
+            <option value="_Password">Password</option>
           </select>
         </div>
       </form>
@@ -34,7 +77,9 @@ export default function UsersInfo() {
             <th>Modify</th>
           </tr>
         </thead>
-        <tbody className='w-100 text-center'></tbody>
+        <tbody className='w-100 text-center'>
+          { tbodyContent }
+        </tbody>
       </table>
       <div className='d-flex justify-content-end px-3'>
         <button type="button" className='btn btn-primary px-2 py-1 mx-2'>a ...</button>
