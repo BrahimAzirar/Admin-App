314eb44bf4c48beb5e26c4ccd951916c5fb18806
Brahim_Azirar <brahimazirar59@gmail.com>
2023-01-10 18:45:57 +0100

    we finish addUsers.js

--- Add_Info/AddUsers.js
+++ Add_Info/AddUsers.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function AddUsers() {
+
+  const FormTarget = useRef();
+
+  function AddData() {
+
+    const myData = new URLSearchParams();
+
+    for (let info of new FormData(FormTarget.current)) {
+      myData.append(info[0], info[1])
+    }
+
+    fetch('http://localhost/appadmin/src/BackEnd/AddUsers.php', {
+      method: "POST",
+      body: myData
+    })
+      .catch(err => console.log(err));
+  }
+
   return (
-    <form className='col-10 h-75 align-self-center justify-content-around align-items-center p-3 border row add'>
+    <form ref={FormTarget} className='col-10 h-75 align-self-center justify-content-around align-items-center p-3 border row add'>
       <h2 className='text-center'>Add User</h2>
       <div className='col-5'>
         <input className='form-control p-1' type="text" name="FirstName" placeholder='First Name'/>
@@ -17,7 +35,7 @@ export default function AddUsers() {
         <input className='form-control p-1' type="password" name="Password" placeholder='Password'/>
       </div>
       <div className='col-10 border'>
-        <button className='btn btn-primary w-100 py-1' type="button">Send</button>
+        <button className='btn btn-primary w-100 py-1' type="button" onClick={AddData}>Send</button>
       </div>
     </form>
   )
--- BackEnd/AddUsers.php
+++ BackEnd/AddUsers.php
@@ -1,3 +1,16 @@
 <?php
 
-    
+    header('Access-Control-Allow-Origin: http://localhost:3000/#');
+    header("Content-Type: JSON");
+
+    $FirstName = $_POST['FirstName'];
+    $LastName = $_POST['LastName'];
+    $Email = $_POST['Email'];
+    $Password = $_POST['Password'];
+
+    $connect = new PDO("mysql:host=localhost:3308; dbname=exo; ", "root", "");
+
+    $Insert = $connect -> prepare("INSERT INTO users (FirstName, LastName, Email, _Password)
+        VALUE ('$FirstName', '$LastName', '$Email', '$Password')");
+
+    $Insert -> execute();
